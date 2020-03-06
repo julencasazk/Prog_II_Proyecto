6115e2775a3a2cbf4df4e1a221c6d1c80707ec07
julencasazk <julencasazk@gmail.com>
2020-03-06 23:04:32 +0100

    Añadido el proyecto

---
+++ Proyecto/src/proyecto/HolaMundo.java
@@ -0,0 +1,11 @@
+package proyecto;
+
+/**
+ * Una prueba de imprimir Hola Mundo por consola, simplemente para empezar el proyecto y asegurar que git funciona bien
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+    }
+}
