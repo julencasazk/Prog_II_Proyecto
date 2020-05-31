ee693f1d2fd6f888e9f3ca34ca60ce5a5048bd80
julencasazk <julen.casal@opendeusto.es>
2020-05-31 13:57:13 +0200

    Cleaned unused imports

--- Proyecto/src/juego/Menu.java
+++ Proyecto/src/juego/Menu.java
@@ -3,9 +3,6 @@ package juego;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.util.ArrayList;
 import java.util.Hashtable;
 
 import javax.swing.BoxLayout;
